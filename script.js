935f676821f36dea07e60ec451224f4481098871
Tushar Raghuvanshi <79913268+T7351@users.noreply.github.com>
2026-02-21 16:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-    const change = document.querySelector('#change_button');
-    const reset = document.querySelector('#reset_button');
-
     const container = document.querySelector(".grid-container");
     for (let i = 1; i <= 9; i++) { 
         const div = document.createElement("div");
         div.className = "grid-item";                 
         div.id = i;                                  
         div.textContent = i;                         
-        container.appendChild(div);https://www.svgrepo.com/show/345221/three-dots.svg
+        container.appendChild(div);
     }
 
+    const change = document.querySelector('#change_button');
+    const reset = document.querySelector('#reset_button');
     change.addEventListener(('click'), (e)=>{
         e.preventDefault();
         const block = document.querySelector('#block_id').value;
