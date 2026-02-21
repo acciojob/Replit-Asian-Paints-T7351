0dee4e37ee1e971b986897c3c03a325d6464a0f4
Tushar Raghuvanshi <79913268+T7351@users.noreply.github.com>
2026-02-21 16:01:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+    const change = document.querySelector('#change_button');
+    const reset = document.querySelector('#Reset');
+
+    const container = document.querySelector("#grid-container");
+    for (let i = 1; i <= 9; i++) { 
+        const div = document.createElement("div");
+        div.className = "grid-item";                 
+        div.id = i;                                  
+        div.textContent = i;                         
+        container.appendChild(div);
+    }
+
+    change.addEventListener(('click'), (e)=>{
+        e.preventDefault();
+        const block = document.querySelector('#block_id').value;
+        const color = document.querySelector('#colour_id').value;
+
+        document.querySelectorAll('.grid-item').forEach(item=>{
+            item.style.backgroundColor = "transparent";
+        });
+        const selected = document.getElementById(block);
+        if(selected){
+            selected.style.backgroundColor = color;
+        }
+    })
+
+    reset.addEventListener(('click'), ()=>{
+        document.querySelectorAll('.grid-item').forEach(item=>{
+            item.style.backgroundColor="grey";
+        });
+    })
